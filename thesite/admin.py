8695b845fcd0e447a6bc514b1b0cd51c83aaf059
brica1000 <brica1000@msn.com>
2017-02-10 23:36:21 +0200

    Added column to admin and working on sending edits to moderator first

--- thesite/admin.py
+++ thesite/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Conglomerate, Product, Cert
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'approved_edit')
+
 admin.site.register(Conglomerate)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Cert)
