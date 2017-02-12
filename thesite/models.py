ec6a704b20f0b76302506058243a4b8a5168d154
brica1000 <brica1000@msn.com>
2017-02-12 12:05:56 +0200

    Still working on checkboxes, updated db datetime to include time

--- thesite/models.py
+++ thesite/models.py
@@ -8,7 +8,7 @@ class Conglomerate(models.Model):  # But each Conglomerate can make many product
     text = models.CharField(max_length=1000)
     sustainability = models.CharField(max_length=1000, blank=True)
     employees = models.CharField(max_length=1000, blank=True)
-    last_updated = models.DateField()
+    last_updated = models.DateTimeField(default=datetime.datetime.now)
     num_stars = models.IntegerField() # out of five
     approved_edit = models.BooleanField(default=False)
 
@@ -38,7 +38,7 @@ class Product(models.Model):  # Each product has one Conglomerate
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     text = models.CharField(max_length=1000)
-    last_updated = models.DateField()
+    last_updated = models.DateTimeField(default=datetime.datetime.now)
     num_stars = models.IntegerField() # out of five
     approved_edit = models.BooleanField(default=False)
 
@@ -56,7 +56,7 @@ class Verification(models.Model):
     conglomerate = models.ForeignKey(Conglomerate, on_delete=models.CASCADE, null=True)
 
     individual = models.CharField(max_length=100, default="", null=True)
-    date = models.DateField(default=datetime.date.today())
+    date = models.DateTimeField(default=datetime.datetime.now)
     who = models.CharField(max_length=1000, default="", null=True)
     comment = models.CharField(max_length=1000, default="", null=True)
     comment_sustain = models.CharField(max_length=1000, default="", null=True)
